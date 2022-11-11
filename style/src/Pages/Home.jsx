6b53b09d6d633a70e26d7f79b61a2e64611a6c45
Rishu jha <jharishu796@gmail>
2022-11-11 09:14:12 +0530

    day-3

---
+++ style/src/Pages/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Carousel from '../Components/Carousel';
+
+
+
+
+const Home = () => {
+  return (
+    <div>
+        <Carousel />
+    </div>
+  )
+}
+
+export default Home
